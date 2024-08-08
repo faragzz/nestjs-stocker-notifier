23df8ee20de918ba31d7eb27ed0486017ae2b393
amir said <amir@enegix.co>
2024-08-09 00:55:40 +0300

    feat: nodeMailer to send emails when got an email

--- src/mail-listener/mail-listener.service.ts
+++ src/mail-listener/mail-listener.service.ts
@@ -5,12 +5,23 @@ import {
   OnModuleInit,
 } from '@nestjs/common';
 import * as MailListener from 'mail-listener2';
+import * as nodemailer from 'nodemailer';
 
 @Injectable()
 export class EmailListenerService implements OnModuleInit, OnModuleDestroy {
   private mailListener: MailListener;
+  private transporter: nodemailer.Transporter;
+
   private readonly logger = new Logger(EmailListenerService.name);
   constructor() {
+    this.transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL, // Your email address
+        pass: process.env.NODE_MAILER_PASSWORD, // Your email password or app-specific password
+      },
+    });
+
     this.mailListener = new MailListener({
       username: process.env.EMAIL,
       password: process.env.PASSWORD,
@@ -36,7 +47,7 @@ export class EmailListenerService implements OnModuleInit, OnModuleDestroy {
         from: mail.from,
         to: mail.to,
         subject: mail.subject,
-        body: mail.text,
+        text: mail.text,
       };
       if (mailFromTarget > -1) {
         this.logger.warn('got email from target');
@@ -46,6 +57,7 @@ export class EmailListenerService implements OnModuleInit, OnModuleDestroy {
         this.logger.log('got public email');
         console.log('New email received Public:', email);
       }
+      this.sendReplyEmail(email);
 
       // Handle the new email
     });
@@ -62,6 +74,22 @@ export class EmailListenerService implements OnModuleInit, OnModuleDestroy {
     this.mailListener.on('end', () => {
       this.logger.warn('Mail Listener has stopped.');
     });
+  }
+
+  private async sendReplyEmail(mail: any) {
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: mail.from[0].address, // Replace with the recipient's email
+      subject: `Re: ${mail.subject}`,
+      text: `Hello, this is an automated reply to your email.\n\nReceived:\n${mail.text}`,
+    };
+
+    try {
+      const info = await this.transporter.sendMail(mailOptions);
+      console.log('Reply email sent:', info.response);
+    } catch (error) {
+      console.error('Error sending email:', error);
+    }
   }
 
   onModuleInit() {
